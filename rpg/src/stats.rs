a28127e63165aca12a7c837798535e1376c27256
drcdh <danielrhunter@gmail.com>
2022-01-06 11:24:34 -0500

    Implement equipment modifiers of derived stats

--- rpg/src/stats.rs
+++ rpg/src/stats.rs
@@ -17,15 +17,23 @@ fn trivial_derived_stat(bs: &BaseStats, key_bs: String) -> Stat {
     *bs.get(&key_bs).unwrap()
 }
 
-fn default_derived_stat(bs: &BaseStats, eq: &EquipmentSet, key_bs: String, key_eq: String) -> Stat {
+fn default_derived_stat(
+    bs: &BaseStats,
+    eq: &EquipmentSet,
+    key_bs: String,
+    key_eq: String,
+    key_ds: String,
+) -> Stat {
     let base_stat: Stat = *bs.get(&key_bs).unwrap();
     let mult_mod: Stat = 1;  // todo, use something like Decimal
     let plus_mod: Stat = eq.into_iter().map(|(_n, i)| item::equipment_mod(&i, &key_bs)).sum();
     let eq_power: Stat = match eq.get(&key_eq) {
-        Some(slot) => item::equipment_power(&slot),
+        Some(slot) => item::equipment_power(slot),
         None => item::equipment_power(&None::<Item>),
     };
-    base_stat*mult_mod + plus_mod + eq_power
+    let ds_mult_mod: Stat = 1;  // todo, use something like Decimal
+    let ds_plus_mod: Stat = eq.into_iter().map(|(_n, i)| item::equipment_mod(&i, &key_ds)).sum();
+    (base_stat*mult_mod + plus_mod + eq_power)*ds_mult_mod + ds_plus_mod
 }
 
 /*  Later, this may become a method of an object BaseStatsGenerator
@@ -39,8 +47,8 @@ pub fn generate_stats() -> (BaseStats, DerivedStats) {
     bs.insert(String::from("Stamina"), 10);
 
     let mut ds = DerivedStats::new();
-    ds.insert(String::from("Offense"), |bs, eq| default_derived_stat(bs, eq, String::from("Strength"), String::from("Weapon")));
-    ds.insert(String::from("Defense"), |bs, eq| default_derived_stat(bs, eq, String::from("Stamina"), String::from("Shield")));
+    ds.insert(String::from("Offense"), |bs, eq| default_derived_stat(bs, eq, String::from("Strength"), String::from("Weapon"), String::from("Offense")));
+    ds.insert(String::from("Defense"), |bs, eq| default_derived_stat(bs, eq, String::from("Stamina"), String::from("Shield"), String::from("Defense")));
     /*
     for (bs_name, _) in &bs {
         ds.insert(bs_name.to_string(), |bs, eq| trivial_derived_stat(bs, bs_name.clone()));

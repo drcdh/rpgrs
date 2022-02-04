1c170dc05cfe8635058d0dc34f9583bc381eedf8
drcdh <danielrhunter@gmail.com>
2022-02-04 06:32:37 -0500

    Add some more fun stuff

--- src/main.rs
+++ src/main.rs
@@ -33,14 +33,25 @@ fn bcli_test<R: Read, W: Write>(stdin: R, stdout: W, ch_enc: &CharacterEncyclope
     };
     cli.run();
 }
+fn bcli_test_boss<R: Read, W: Write>(stdin: R, stdout: W, ch_enc: &CharacterEncyclopedia) {
+    let mut allies = Party::new("Allies".to_string());
+    allies.add_clone(&IndexedOrLiteral::Index(515), ch_enc);
+    allies.add_clone(&IndexedOrLiteral::Index(521), ch_enc);
+    allies.add_clone(&IndexedOrLiteral::Index(619), ch_enc);
 
-fn main() {
-    let phonebook = CharacterEncyclopedia::new("data/characters.json");
+    let mut baddies = Party::new("Baddies".to_string());
+    baddies.add_clone(&IndexedOrLiteral::Index(103), ch_enc);
 
-    let termsize = termion::terminal_size().ok();
-    let termwidth = termsize.map(|(w,_)| w - 2).unwrap();
-    let termheight = termsize.map(|(_,h)| h - 2).unwrap();
+    let battle = Battle::new(allies, baddies);
+    let mut cli = BattleCLI {
+        stdin: stdin.keys(),
+        stdout,
+        battle,
+    };
+    cli.run();
+}
 
+fn easy_fight(phonebook: &CharacterEncyclopedia) {
     let stdout = io::stdout();
     let stdout = stdout.lock();
     let stdin = io::stdin();
@@ -50,14 +61,30 @@ fn main() {
     let stdout = stdout.into_raw_mode().unwrap();
 
     bcli_test(stdin, stdout, &phonebook);
-/*
-    let allies = Party::new("Allies");
-    let baddies = Party::new("Baddies");
-    let mut battle = battle::Battle {allies, baddies}
-*/
-    
-    //let answer = menu_test();
-    //print!("{}Got answer {}", Goto(15, 15), answer);
+}
+
+fn boss_fight(phonebook: &CharacterEncyclopedia) {
+    let stdout = io::stdout();
+    let stdout = stdout.lock();
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
+
+    // We go to raw mode to make the control over the terminal more fine-grained.
+    let stdout = stdout.into_raw_mode().unwrap();
+
+    bcli_test_boss(stdin, stdout, &phonebook);
+}
+
+fn main() {
+    let phonebook = CharacterEncyclopedia::new("data/characters.json");
+
+    let termsize = termion::terminal_size().ok();
+    let termwidth = termsize.map(|(w,_)| w - 2).unwrap();
+    let termheight = termsize.map(|(_,h)| h - 2).unwrap();
+
+    easy_fight(&phonebook);
+    boss_fight(&phonebook);
+
     print!("{}{}{}", ClearAll, termion::style::Reset, Goto(1, 1));
     println!("Terminal width, height is ({}, {})", termwidth, termheight);
 }

726076c08a95b96b1e9ed90aed7b2ecd5ce0951c
drcdh <danielrhunter@gmail.com>
2022-09-17 04:55:59 -0400

    Implement players_guide using clap for command line argument parsing

---
+++ examples/players_guide.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+
+use rpgrs::common::*;
+use rpgrs::encyclopedia::CharacterEncyclopedia;
+
+#[derive(Parser, Debug)]
+#[clap(version, about)]
+struct Args {
+//    #[clap(long)]
+    id: u64,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let id = args.id as Id;
+
+//    let id = std::env::args().nth(1).expect("no Id given");
+
+    let phonebook = CharacterEncyclopedia::new("data/characters.json");
+    
+    let c = phonebook.get(&id).expect(&format!("no character with Id={id}").to_string()); 
+    
+    println!("{}", c);
+}
